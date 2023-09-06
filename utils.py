8b38d904df960ad2e0e8dcc5e784e372d9864737
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-09-06 09:07:00 -0500

    Model test

--- utils.py
+++ utils.py
@@ -11,24 +11,53 @@ warnings.filterwarnings("ignore")
 # DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Net(nn.Module):
-  """Model (simple CNN adapted from 'PyTorch: A 60 Minute Blitz')"""
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3,   64,  3)
+        self.conv2 = nn.Conv2d(64,  128, 3)
+        self.conv3 = nn.Conv2d(128, 256, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.drop1 = nn.Dropout2d(p=0.5)
+        self.fc1 = nn.Linear(64 * 4 * 4, 128)
+        self.drop2 = nn.Dropout2d(p=0.5)
+        self.fc2 = nn.Linear(128, 256)
+        self.drop3 = nn.Dropout2d(p=0.5)
+        self.fc3 = nn.Linear(256, 10)
 
-  def __init__(self) -> None:
-    super(Net, self).__init__()
-    self.conv1 = nn.Conv2d(3, 6, 5)
-    self.pool = nn.MaxPool2d(2, 2)
-    self.conv2 = nn.Conv2d(6, 16, 5)
-    self.fc1 = nn.Linear(16 * 5 * 5, 120)
-    self.fc2 = nn.Linear(120, 84)
-    self.fc3 = nn.Linear(84, 10)
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = x.view(-1, 64 * 4 * 4)
+        x = self.drop1(x)
+        x = F.relu(self.fc1(x))
+        x = self.drop2(x)
+        x = F.relu(self.fc2(x))
+        x = self.drop3(x)
+        x = self.fc3(x)
+        return x
 
-  def forward(self, x: torch.Tensor) -> torch.Tensor:
-    x = self.pool(F.relu(self.conv1(x)))
-    x = self.pool(F.relu(self.conv2(x)))
-    x = x.view(-1, 16 * 5 * 5)
-    x = F.relu(self.fc1(x))
-    x = F.relu(self.fc2(x))
-    return self.fc3(x)
+
+
+#class Net(nn.Module):
+#  """Model (simple CNN adapted from 'PyTorch: A 60 Minute Blitz')"""#
+
+#  def __init__(self) -> None:
+#    super(Net, self).__init__()
+#    self.conv1 = nn.Conv2d(3, 6, 5)
+#    self.pool = nn.MaxPool2d(2, 2)
+#    self.conv2 = nn.Conv2d(6, 16, 5)
+#    self.fc1 = nn.Linear(16 * 5 * 5, 120)
+#    self.fc2 = nn.Linear(120, 84)
+#    self.fc3 = nn.Linear(84, 10)
+
+#  def forward(self, x: torch.Tensor) -> torch.Tensor:
+#    x = self.pool(F.relu(self.conv1(x)))
+#    x = self.pool(F.relu(self.conv2(x)))
+#    x = x.view(-1, 16 * 5 * 5)
+#    x = F.relu(self.fc1(x))
+#    x = F.relu(self.fc2(x))
+#    return self.fc3(x)
 
 
 def load_data():
