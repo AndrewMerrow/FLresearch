04efba317748733eb7420816b11509c6ad8a618d
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-09-26 13:07:24 -0500

    Server poison config

--- server.py
+++ server.py
@@ -178,7 +178,7 @@ def main():
     # Start Flower server for four rounds of federated learning
     fl.server.start_server(
         server_address="10.100.116.10:8080",
-        config=fl.server.ServerConfig(num_rounds=2),
+        config=fl.server.ServerConfig(num_rounds=10),
         strategy=strategy,
     )
 
