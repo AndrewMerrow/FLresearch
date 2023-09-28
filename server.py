d1fe6ce97655f6739cd7038084856392828c2d7f
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-09-27 21:27:22 -0500

    Small test config

--- server.py
+++ server.py
@@ -175,9 +175,9 @@ def main():
     # Create strategy
     #strategy = fl.server.strategy.FedAvg(
     strategy = AggregateCustomMetricStrategy(
-        min_fit_clients=10,
-        min_evaluate_clients=10,
-        min_available_clients=10,
+        min_fit_clients=2,
+        min_evaluate_clients=2,
+        min_available_clients=2,
         evaluate_fn=get_evaluate_fn(model, args.toy),
         on_fit_config_fn=fit_config,
         on_evaluate_config_fn=evaluate_config,
